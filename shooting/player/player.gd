846a000de15dc6009c2897f20e804dd8be727d9a
225zz5 <usiwaka74@docomo.ne.jp>
2025-06-28 09:03:08 +0900

    feat:プレイヤーの操作を作った

---
+++ shooting/player/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+const SPEED = 200
+
+#座標管理
+func _physics_process(delta):
+	var input_velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_left") or Input.is_action_pressed("ui_left"):
+		input_velocity.x -= 1
+	if Input.is_action_pressed("move_right") or Input.is_action_pressed("ui_right"):
+		input_velocity.x += 1
+
+	#実際に動かす処理
+	velocity = input_velocity * SPEED
+	move_and_slide()
