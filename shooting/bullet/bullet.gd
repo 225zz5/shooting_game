16be1992fc543c59c3cc47aaeb585b9284cd73ec
225zz5 <usiwaka74@docomo.ne.jp>
2025-06-28 11:04:05 +0900

    feat: playerが出す弾を追加した

--- shooting/bullet/bullet.gd
+++ shooting/bullet/bullet.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
-const SPEED = 400
-
+const SPEED :int = 400
+const rotation_bullt :int = 5
 func _physics_process(delta):
+	rotation += deg_to_rad(rotation_bullt)
 	velocity = Vector2(0, -SPEED)	# 上に進むように
 	move_and_slide()
 	
--- shooting/player/player.gd
+++ shooting/player/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
-const SPEED = 300
+const SPEED :int = 300
+const BULLET_SCENE :PackedScene = preload("res://bullet/bullet.tscn")
+@onready var time_shot :Timer = $Timer_shot
 
-#座標管理
+#playerの座標管理
 func _physics_process(delta):
 	var input_velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_left") or Input.is_action_pressed("ui_left"):
@@ -10,6 +12,24 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_right") or Input.is_action_pressed("ui_right"):
 		input_velocity.x += 1
 
-	#実際に動かす処理
+	#playerを実際に動かす処理
 	velocity = input_velocity * SPEED
 	move_and_slide()
+	
+
+	#0.5秒おきに弾が出る処理
+	if Input.is_action_pressed("shot"):
+		if time_shot.is_stopped():
+			time_shot.start()
+	else:
+		time_shot.stop()
+
+func _on_timer_shot_timeout():
+	if Input.is_action_pressed("shot"):
+		bullet()
+	
+#弾の処理
+func bullet():
+			var bullet = BULLET_SCENE.instantiate()
+			bullet.global_position = global_position + Vector2(0, -20)
+			get_tree().current_scene.add_child(bullet)
